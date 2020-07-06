bc1e81a9c42a23f7e427d2bc9856e77cfe89caa5
bittencourt-lucas <lucascafieiro@hotmail.com>
2020-07-06 08:45:51 -0300

    Primeira parte front-end finalizada

--- aulas/06-gobarber-web/src/pages/SignUp/index.tsx
+++ aulas/06-gobarber-web/src/pages/SignUp/index.tsx
@@ -3,6 +3,11 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -11,12 +16,20 @@ import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -31,37 +44,58 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
+
+      await api.post('/users', data);
+
+      history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro realizado com sucesso',
+        description: 'Você já pode fazer login!',
+      });
+
     } catch (err) {
-      console.log(err);
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
 
-      const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
 
-      formRef.current?.setErrors(errors);
+        return;
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro no cadastro',
+        description: 'Suas informações são inválidas.',
+      });
     }
-  }, []);
+  }, [addToast, history]);
 
   return (
     <Container>
     <Background />
     <Content>
-      <img src={logoImg} alt="GoBarber" />
+      <AnimationContainer>
+        <img src={logoImg} alt="GoBarber" />
 
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Faça seu cadastro</h1>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-        <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-        <Button type="submit">Cadastrar</Button>
-      </Form>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para a tela de login
-      </a>
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para a tela de login
+        </Link>
+      </AnimationContainer>
     </Content>
   </Container>
   )
