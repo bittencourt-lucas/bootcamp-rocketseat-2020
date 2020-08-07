5ee71ec5594e9f00fe8de3d12fbde5508c98071e
bittencourt-lucas <lucascafieiro@hotmail.com>
2020-08-06 23:54:48 -0300

    Testes de appointments feitos

--- aulas/08-arquitetura-ddd/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ aulas/08-arquitetura-ddd/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,42 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
+
+  it('should not be able to create two appointments on the same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appoitnmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appoitnmentDate,
+      provider_id: '123123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appoitnmentDate,
+        provider_id: '123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
