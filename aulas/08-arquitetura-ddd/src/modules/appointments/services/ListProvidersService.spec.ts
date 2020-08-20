f160b913afc740aabb26f2685df131fc2e7b4939
bittencourt-lucas <lucascafieiro@hotmail.com>
2020-08-19 21:32:47 -0300

    Listagem de providers

---
+++ aulas/08-arquitetura-ddd/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,39 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ShowProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to show the profile', async () => {
+    const johndoe = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const janedoe = await fakeUsersRepository.create({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Wick',
+      email: 'johnwick@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([johndoe, janedoe]);
+  });
+});
