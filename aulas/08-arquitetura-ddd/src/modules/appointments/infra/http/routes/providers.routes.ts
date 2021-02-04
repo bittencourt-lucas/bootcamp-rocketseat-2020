7bdaa722e79e99f90d4be06a12e90caf4ac105e7
bittencourt-lucas <lucascafieiro@hotmail.com>
2021-02-04 10:31:59 -0300

    Rotas e controllers

--- aulas/08-arquitetura-ddd/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ aulas/08-arquitetura-ddd/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailability = new ProviderMonthAvailabilityController();
+const providerDayAvailability = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailability.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailability.index,
+);
 
 export default providersRouter;
