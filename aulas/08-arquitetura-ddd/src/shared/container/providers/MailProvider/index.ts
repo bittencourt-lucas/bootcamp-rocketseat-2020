34b4f79b9a86b568230e3b1e83f22ca44453ec45
bittencourt-lucas <lucascafieiro@hotmail.com>
2021-02-06 16:55:33 -0300

    Organizando o container e Configurando cache

---
+++ aulas/08-arquitetura-ddd/src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
+const providers = {
+  ethereal: EtherealMailProvider,
+};
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve(providers.ethereal),
+);
